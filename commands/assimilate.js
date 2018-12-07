8f5d4e32059166a5c0c0330a663b92cca40205c1
Keshav <kgupt1@ocdsb.ca>
2018-12-06 20:04:45 -0500

    updated assimilate command

--- commands/assimilate.js
+++ commands/assimilate.js
@@ -3,7 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args)=>{
   let contenttoreply = message.mentions.users.first() || args[0]
   if (contenttoreply.id === "444998388795179042" || contenttoreply.id === "518206536535769099") return message.reply("Feeble human! You would attempt to assimilate The Borg Collective?")
-  message.channel.send(`${contenttoreply} http://www.youtube.com/watch?v=AyenRCJ_4Ww`)
+  let assimilatemessage = args.join(" ").slice(22);
+  if (!assimilatemessage){
+    assimilatemessage = "You have just been assimilated!"
+  }
+  message.channel.send(`${contenttoreply}, ${assimilatemessage} http://www.youtube.com/watch?v=AyenRCJ_4Ww`)
 }
 
 module.exports.help = {
