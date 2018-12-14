4e3f9e2575d02899a3629fa78c764d0c1ff47dad
Keshav <kgupt1@ocdsb.ca>
2018-12-13 20:30:09 -0500

    improved botinfo

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,13 +1,22 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args)=>{
-
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setTitle("Bot Information")
-    .setColor("GREEN")
-    .setThumbnail(bicon)
-    .addField("Bot Name:", bot.user.username)
+  let bicon = bot.user.displayAvatarURL;
+  let Unum = bot.users.size
+  let Cnum = bot.channels.size
+  let Snum = bot.guilds.size
+  let botembed = new Discord.RichEmbed()
+  .setColor("GREEN")
+  .setThumbnail(bicon)
+  .setDescription("Bot Info")
+  .addField("Creator", "<@444998388795179042>")
+  .addField("Bot Name", `${bot.user.username}`)
+  .addField("Servers", `${Snum}`)
+  .addField("Channels", `${Cnum}`)
+  .addField("Users", `${Unum}`)
+  .addField("Created On", bot.user.createdAt)
+  .setTimestamp()
+  .addField("Git Repo", "https://github.com/Kggupta/borgcube")
 
     message.channel.send(botembed);
 }
