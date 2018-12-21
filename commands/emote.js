7a0bb4efe4f12fdeb1a05266140aae3207738b15
Keshav <kgupt1@ocdsb.ca>
2018-12-21 18:55:44 -0500

    updates to description

--- commands/emote.js
+++ commands/emote.js
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args)=>{
 module.exports.help = {
   name: "emote",
   aliases: ["e"],
-  description: "Have the bot send an animated emote on your behalf!\n Type `brbug {Emote URL}` to have it added to the list of emotes **Must be animated**.\nType `bhelp` to see the list of emotes that the bot currently has.",
+  description: "\n-Have the bot send an animated emote on your behalf!\n-Type `brbug {Emote URL}` to have it added to the list of emotes **Must be animated**.\n-Type `bhelp` to see the list of emotes that the bot currently has.",
   usage: "bemote {name}",
   accessibleby: "Anyone"
 }
