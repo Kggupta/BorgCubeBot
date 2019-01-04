262968326d37ca4c60a4dbecef62096e621f3f66
Keshav <kgupt1@ocdsb.ca>
2019-01-04 11:43:47 -0500

    new emote

--- commands/emote.js
+++ commands/emote.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args)=>{
-  const emotes = ["jelly", "<a:jelly:525727683506864148>", "mario", "<a:mario:525727683305537548>", "smak", "<a:smak:525727716805443604>", "sanity", "<a:sanity:525727716520230950>", "partybird", "<a:partybird:525727685545164810>","penguin", "<a:penguin1:525739937128120340><a:penguin2:525739937342160896><a:penguin3:525739937195360257><a:penguin4:525739937379647488>", "scared", "<a:scared:525740761262587904>","dance", "<a:dance:525697744711712771>","nom","<a:nom:525745634842771476>", "neonblob", "<a:neonblob:525835833388040258>", "jellywall", "<a:jellywall:529040908939952128>"];
+  const emotes = ["jelly", "<a:jelly:525727683506864148>", "mario", "<a:mario:525727683305537548>", "smak", "<a:smak:525727716805443604>", "sanity", "<a:sanity:525727716520230950>", "partybird", "<a:partybird:525727685545164810>","penguin", "<a:penguin1:525739937128120340><a:penguin2:525739937342160896><a:penguin3:525739937195360257><a:penguin4:525739937379647488>", "scared", "<a:scared:525740761262587904>","dance", "<a:dance:525697744711712771>","nom","<a:nom:525745634842771476>", "neonblob", "<a:neonblob:525835833388040258>", "jellywall", "<a:jellywall:529040908939952128>", "dogedance", "<a:dogedance:530787990386114570>"];
   emoteName = args[0];
   if(!emoteName) return;
   const isInArray = emotes.includes(emoteName);
