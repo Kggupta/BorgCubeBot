85cb0c1745422f8a4ee5ac46a061cd77e5693d23
Keshav <kgupt1@ocdsb.ca>
2019-01-29 17:32:49 -0500

    bug fixes

--- commands/purge.js
+++ commands/purge.js
@@ -7,13 +7,10 @@ module.exports.run = async (bot, message, args)=>{
     }
 
     var am = args[0]
-    message.channel.send("Assimilating: " + am + " messages").then(m => m.delete(2000))
-
     setTimeout(() => {
-      message.channel.bulkDelete(am)
-      .then(() => {
-          message.channel.send("Just assimilated: " + am + " messages.").then(m => m.delete(1500))
-      })}, 1000);
+      message.channel.bulkDelete(am).catch(err => message.channel.send("I couldn't assimilate those messages. Bots cannot purge more than 100 messages at a time nor can they delete messages more than 14 days old. You may also get this message due to a DiscordAPI overload.").then(m => m.delete(2000)))
+      message.channel.send("I've assimilated out  " + am + " messages.").then(m => m.delete(1000))
+    }, 2000);
   } else {
   	message.channel.send("Feeble human you do not have permission to do that!").then(m => m.delete(1500))
   }
@@ -24,5 +21,5 @@ module.exports.help = {
   aliases: ["delete", "del"],
   description: "Delete a specified amount of messages in a channel.\n- Bot can only purge 100 messages at a time\n - Messages older than 14 days cannot be deleted. This is due to Discord ToS and cannot be avoided.",
   usage: "bpurge {amount}",
-  accessibleby: "Must have `MANAGE_MESSAGES` permission."
+  accessibleby: "`MANAGE_MESSAGES`"
 }
