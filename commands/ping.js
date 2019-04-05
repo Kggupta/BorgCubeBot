ed588bb67d44c667b88920b7f3aef9766806ba7c
Keshav <kgupt1@ocdsb.ca>
2019-04-05 16:57:16 -0400

    improved ping command to be less bulky

--- commands/ping.js
+++ commands/ping.js
@@ -1,14 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args)=>{
-    let basestation = Math.floor(Math.random()*1000)+1;
     let ping = Math.round(bot.ping)
-    let picon = message.author.displayAvatarURL;
+
     let pingembed = new Discord.RichEmbed()
-    .setTitle("Hailing Frequencies = INITIATED")
+    .setTitle("Hailing Frequencies")
     .setColor("GREEN")
-    .setThumbnail(picon)
-    .addField(`Reply recieved from Starbase-${basestation}`, `Positron-delay: ${ping}ms`)
+    .setDescription(`Positron-delay: ${ping}ms`)
 
     message.channel.send(pingembed);
 }
