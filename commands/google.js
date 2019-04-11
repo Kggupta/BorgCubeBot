af146369fbd9b21c24e36b75a7c4201b8bb8341b
Keshav <kgupt1@ocdsb.ca>
2019-04-11 18:05:30 -0400

    google command

---
+++ commands/google.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const google = require("google");
+
+module.exports.run = async (bot, message, args)=>{
+  google.resultsPerPage = 25
+  let description = "";
+  google(args.join(" "), function (err, res){
+    if (err) console.error(err)
+      var link = res.links[1];
+      console.log(link.title + ' - ' + link.href)
+      console.log(link.description + "\n")
+      if(!link.description){
+        description = "None given"
+      }else{
+        description = link.description
+      }
+
+      let googleEmbed = new Discord.RichEmbed()
+      .setTitle(link.title)
+      .setColor("GREEN")
+      .addField(`Description`, description)
+      .addField(`Link`, link.href)
+
+      return message.channel.send(googleEmbed);
+  })
+}
+
+module.exports.help = {
+  name: "google",
+  aliases: ["goo"],
+  description: "Search Google! - BETA",
+  usage: "bgoogle {search}",
+  accessibleby: "Anyone"
+}
