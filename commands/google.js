7d63c41c6c22d1c3306a9a50f7ea830b02af901a
Kggupta <kgupt1@ocdsb.ca>
2019-12-26 10:03:25 -0500

    added catch to google search

--- commands/google.js
+++ commands/google.js
@@ -4,23 +4,27 @@ const google = require("google");
 module.exports.run = async (bot, message, args)=>{
   google.resultsPerPage = 25
   let description = "";
-  google(args.join(" "), function (err, res){
-    if (err) console.error(err)
-      var link = res.links[1];
-      if(!link.description){
-        description = "None given"
-      }else{
-        description = link.description
-      }
+  try{
+    google(args.join(" "), function (err, res){
+      if (err) console.error(err)
+        var link = res.links[1];
+        if(!link.description){
+          description = "None given"
+        }else{
+          description = link.description
+        }
 
-      let googleEmbed = new Discord.RichEmbed()
-      .setTitle(link.title)
-      .setColor("GREEN")
-      .addField(`Description`, description)
-      .addField(`Link`, link.href)
+        let googleEmbed = new Discord.RichEmbed()
+        .setTitle(link.title)
+        .setColor("GREEN")
+        .addField(`Description`, description)
+        .addField(`Link`, link.href)
 
-      return message.channel.send(googleEmbed);
-  })
+        message.channel.send(googleEmbed);
+    })
+  }catch(e){
+    message.channel.send("Google is blocking discord API's from interacting with it. I'm currently looking for a solution, sorry for the inconvenience!")
+  }
 }
 
 module.exports.help = {
