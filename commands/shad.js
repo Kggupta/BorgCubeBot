a756a489ac3189b7d56c69682c60364701a79996
Kggupta <kgupt1@ocdsb.ca>
2020-02-16 10:48:09 -0500

    enabled shad command for all admins

--- commands/shad.js
+++ commands/shad.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args)=>{
   if (message.guild.id != "546754678172549124") return;
-  if (message.author.id != "444998388795179042") return message.channel.send("This command is disabled for you. Please wait for a mod.")
+  if (!member.hasPermission("ADMINISTRATOR")) return message.channel.send("This command is disabled for you. Please wait for a mod.")
   message.channel.send("Passed to compiler");
   let nameOfUser = message.guild.member(message.mentions.users.first());
   if (!nameOfUser) return message.channel.send("You need to add a name.");
@@ -22,7 +22,7 @@ module.exports.run = async (bot, message, args)=>{
 
 module.exports.help = {
   name: "shad",
-  aliases: [],
+  aliases: ["sms"],
   description: "Add your social media's to the compiled list of social media channel. See `usage` to see how to use this command. If you only have one of the two accounts, simply say N/A for that account.",
   usage: "bshad <member> [snap account] [insta account]",
   accessibleby: "The Borg"
